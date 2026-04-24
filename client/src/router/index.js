be3fa69cc0bb404dad9487b283f8fb4b33874676
Mathias BERAMICE-DRACAN <brotox9@gmail.com>
2026-04-24 19:35:57 +0200

    ajout de bouton vie

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,7 +5,6 @@ import RegisterPage from '@/pages/RegisterPage.vue'
 import { hasRole, hasToken } from '@/utils/api'
 import { createRouter, createWebHistory } from 'vue-router'
 import { hasToken, hasRole } from '@/utils/api'
-
 import JoueurPage from '@/pages/JoueurPage.vue'
 
 const routes = [

b7ea0ff58b6605705909d6bad3b2e2b8411888a7
Mathias BERAMICE-DRACAN <brotox9@gmail.com>
2026-04-24 19:35:57 +0200

    ajout de bouton vie

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import MaitreJeuViewPage from '@/pages/MaitreJeuViewPage.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { hasToken, hasRole } from '@/utils/api'
+
 import JoueurPage from '@/pages/JoueurPage.vue'
 
 const routes = [
@@ -14,6 +15,8 @@ const routes = [
   { path: '/login', name: 'Login', component: LoginPage },
   { path: '/register', name: 'Register', component: RegisterPage },
   { path: '/gamemaster/dashboard', name: 'MaitreJeuView', component: MaitreJeuViewPage },
+  { path: '/MaitreJeuView', name: 'MaitreJeuView', component: MaitreJeuViewPage },
+  { path: '/joueur', name: 'Joueur', component: JoueurPage },
 ]
 
 export default createRouter({

0c2f64d92c091a44ea85737537283ace7d6d4baf
uZLaXe <dewasmea@3il.fr>
2026-04-26 23:58:09 +0200

    party-minor-fix

--- client/src/components/PartyCard.vue
+++ client/src/components/PartyCard.vue
@@ -1,38 +1,22 @@
 <!-- component for party cards -->
 <script setup>
-import api from '@/utils/api'
-import { onMounted, ref } from 'vue'
-
-const parties = ref([])
-
-onMounted(() => {
-  api
-    .get('/party/list')
-    .then((res) => {
-      parties.value = res.data
-      console.log(parties.value)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+defineProps({
+  partyId: { type: Number },
+  partyMjId: { type: Number },
 })
 </script>
 
 <template>
   <!-- card -->
-  <div
-    class="bg-green-jdr rounded-lg shadow-md p-8 w-full max-w-xs border-olive-jdr/40 border-2"
-    v-for="party in parties"
-    :key="party.id"
-  >
+  <div class="bg-green-jdr rounded-lg shadow-md p-8 w-full max-w-xs border-olive-jdr/40 border-2">
     <!-- party id -->
     <h1 class="text-2xl text-creamy-jdr mb-2 font-bold bg-orange-jdr rounded-lg px-2">
-      Partie #{{ party.id }}
+      Partie #{{ partyId }}
     </h1>
     <!-- party owner -->
     <h2 class="text-xl text-creamy-jdr font-semibold p-2">Hébergée par:</h2>
     <h2 class="text-xl text-creamy-jdr font-semibold p-2 text-center bg-orange-jdr rounded-lg">
-      {{ party.mj_id }}
+      {{ partyMjId }}
     </h2>
     <!-- party population -->
     <p class="text-creamy-jdr p-2">Nombre de joueurs: 5<!-- {{ party.character_count }}--></p>
--- client/src/pages/PartiesPage.vue
+++ client/src/pages/PartiesPage.vue
@@ -1,11 +1,28 @@
 <script setup>
 import pcard from '@/components/PartyCard.vue'
+
+import api from '@/utils/api'
+import { onMounted, ref } from 'vue'
+
+const parties = ref([])
+
+onMounted(() => {
+  api
+    .get('/party/list')
+    .then((res) => {
+      parties.value = res.data
+      console.log(parties.value)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 </script>
 
 <template>
   <!-- main container -->
-  <div class="flex flex-wrap p-4 gap-4 justify-center">
-    <pcard />
+  <div class="flex flex-wrap p-4 gap-4 justify-center" v-for="party in parties" :key="party.id">
+    <pcard :partyId="party.id" :partyMjId="party.mj_id" />
   </div>
 </template>
 

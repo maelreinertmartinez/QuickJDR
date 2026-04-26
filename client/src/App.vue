24cb77a51d1364e8d724aa54bd7e6e507ab11afe
uZLaXe <dewasmea@3il.fr>
2026-04-26 23:32:42 +0200

    logout-btn-shadow

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,7 +21,7 @@ const logout = () => {
 <template>
   <button
     v-if="hasToken()"
-    class="absolute right-4 top-4 bg-orange-jdr text-creamy-jdr px-3 py-2 rounded-lg hover:bg-brown-jdr"
+    class="absolute right-4 top-4 bg-orange-jdr text-creamy-jdr px-3 py-2 rounded-lg hover:bg-brown-jdr shadow-2xl"
     @click="logout"
   >
     Déconnexion

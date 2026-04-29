7acef6259e3342db22610de9eb28315a6f3ffc27
uZLaXe <dewasmea@3il.fr>
2026-04-29 20:12:40 +0200

    party creation page update 2 / player adding feature

--- client/src/pages/PartyCreationPage.vue
+++ client/src/pages/PartyCreationPage.vue
@@ -19,32 +19,34 @@ onMounted(() => {
 </script>
 
 <template>
-  <pscroll />
-  <div class="min-h-screen flex items-center justify-center flex-col">
-    <div
-      v-if="hasRole('game_master')"
-      class="bg-green-jdr rounded-lg shadow-md p-8 w-full max-w-xs border-olive-jdr/40 border-2 text-center"
-    >
-      <h1 class="text-2xl text-creamy-jdr mb-2 font-bold px-2">
-        Partie n°{{ partyInfo.id }} crée avec succès
-      </h1>
-      <div class="flex flex-col gap-3">
-        <a
-          v-if="hasRole('game_master')"
-          href="/party/list"
-          class="mt-4 mb-2 bg-orange-jdr text-creamy-jdr px-1 py-2 rounded-lg hover:bg-brown-jdr"
-        >
-          Retour à la liste des parties
-        </a>
-        <a
-          v-if="hasRole('game_master')"
-          :href="`/party/${partyInfo.id}`"
-          class="mt-4 mb-2 bg-orange-jdr text-creamy-jdr px-1 py-2 rounded-lg hover:bg-brown-jdr"
-        >
-          Dashboard de la partie
-        </a>
+  <div class="min-h-screen flex flex-row flex-wrap items-center justify-center gap-4">
+    <div class="flex flex-col">
+      <div
+        v-if="hasRole('game_master')"
+        class="bg-green-jdr rounded-lg shadow-md p-8 w-full max-w-xs border-olive-jdr/40 border-2 text-center"
+      >
+        <h1 class="text-2xl text-creamy-jdr mb-2 font-bold px-2">
+          Partie n°{{ partyInfo.id }} créee avec succès
+        </h1>
+        <div class="flex flex-col gap-3">
+          <a
+            v-if="hasRole('game_master')"
+            href="/party/list"
+            class="mt-4 mb-2 bg-orange-jdr text-creamy-jdr px-1 py-2 rounded-lg hover:bg-brown-jdr"
+          >
+            Retour à la liste des parties
+          </a>
+          <a
+            v-if="hasRole('game_master')"
+            :href="`/party/${partyInfo.id}`"
+            class="mt-4 mb-2 bg-orange-jdr text-creamy-jdr px-1 py-2 rounded-lg hover:bg-brown-jdr"
+          >
+            Dashboard de la partie
+          </a>
+        </div>
       </div>
     </div>
+    <pscroll :partyId="partyInfo.id" />
   </div>
 </template>
 
